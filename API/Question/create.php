2674d740f1c4aea4dfed40d9baa1a90c317751ef
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2022-06-28 07:56:42 +0700

    Feat: create Create API

---
+++ API/Question/create.php
@@ -0,0 +1,33 @@
+<?php
+
+    header('Access-Control-Allow-Origin:*');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Headers:Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization,X-Requested-With');
+
+    include_once $_SERVER['DOCUMENT_ROOT'].'/RestFullAPI_App/Config/db.php';
+    include_once $_SERVER['DOCUMENT_ROOT'].'/RestFullAPI_App/Model/question.php';
+
+    $db = new db();
+    $connect = $db->connect();
+
+    $question = new Question($connect);
+
+    $data = json_decode(file_get_contents("php://input"));
+
+    $question->TITLE = $data->title;
+    $question->OPTION_A = $data->option_a;
+    $question->OPTION_B = $data->option_b;
+    $question->OPTION_C = $data->option_c;
+    $question->OPTION_D = $data->option_d;
+    $question->RIGHT_ANSWER = $data->right_answer;
+
+    if($question->create()){
+        echo json_encode(array('message','Question was created'));
+    }
+    else{
+        echo json_encode(array('message','Question was not created'));
+    }
+
+
+?>
--- Model/question.php
+++ Model/question.php
@@ -34,7 +34,7 @@ class Question{
         $stmt = $this->conn->prepare($query);
         $stmt->bindParam(1, $this->ID_QUESTION);
         $stmt->execute();
-        
+
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
         $this->TITLE = $row['TITLE'];
@@ -46,6 +46,67 @@ class Question{
         
     }
 
+    public function create(){
+        $query = "INSERT INTO tbl_question SET TITLE=:TITLE, OPTION_A=:OPTION_A,
+                              OPTION_B=:OPTION_B, OPTION_C=:OPTION_C, OPTION_D=:OPTION_D, RIGHT_ANSWER=:RIGHT_ANSWER";
+        $stmt = $this->conn->prepare($query);
+
+        //clean data
+        $this->TITLE = htmlspecialchars(strip_tags($this->TITLE));
+        $this->OPTION_A = htmlspecialchars(strip_tags($this->OPTION_A));
+        $this->OPTION_B = htmlspecialchars(strip_tags($this->OPTION_B));
+        $this->OPTION_C = htmlspecialchars(strip_tags($this->OPTION_C));
+        $this->OPTION_D = htmlspecialchars(strip_tags($this->OPTION_D));
+        $this->RIGHT_ANSWER = htmlspecialchars(strip_tags($this->RIGHT_ANSWER));
+
+        //bind data
+        $stmt->bindParam(':TITLE', $this->TITLE);
+        $stmt->bindParam(':OPTION_A', $this->OPTION_A);
+        $stmt->bindParam(':OPTION_B', $this->OPTION_B);
+        $stmt->bindParam(':OPTION_C', $this->OPTION_C);
+        $stmt->bindParam(':OPTION_D', $this->OPTION_D);
+        $stmt->bindParam(':RIGHT_ANSWER', $this->RIGHT_ANSWER);
+
+
+        if($stmt->execute()){
+            return true;
+        }
+
+        printf("ERROR %s.\n",$stmt->error);
+        return false;
+
+    }
+
+    public function update(){
+        $query = "UPDATE tbl_question SET TITLE=:TITLE, OPTION_A=:OPTION_A, OPTION_B=:OPTION_B, 
+                  OPTION_C=:OPTION_C, OPTION_D=:OPTION_D, RIGHT_ANSWER=:RIGHT_ANSWER WHERE ID_QUESTION = ?";
+        $stmt = $this->conn->prepare($query);
+
+        //clean data
+        $this->TITLE = htmlspecialchars(strip_tags($this->TITLE));
+        $this->OPTION_A = htmlspecialchars(strip_tags($this->OPTION_A));
+        $this->OPTION_B = htmlspecialchars(strip_tags($this->OPTION_B));
+        $this->OPTION_C = htmlspecialchars(strip_tags($this->OPTION_C));
+        $this->OPTION_D = htmlspecialchars(strip_tags($this->OPTION_D));
+        $this->RIGHT_ANSWER = htmlspecialchars(strip_tags($this->RIGHT_ANSWER));
+
+        //bind data
+        $stmt->bindParam(':TITLE', $this->TITLE);
+        $stmt->bindParam(':OPTION_A', $this->OPTION_A);
+        $stmt->bindParam(':OPTION_B', $this->OPTION_B);
+        $stmt->bindParam(':OPTION_C', $this->OPTION_C);
+        $stmt->bindParam(':OPTION_D', $this->OPTION_D);
+        $stmt->bindParam(':RIGHT_ANSWER', $this->RIGHT_ANSWER);
+        $stmt->bindParam(':ID_QUESTION', $this->ID_QUESTION);
+
+        if($stmt->execute()){
+            return true;
+        }
+
+        printf("ERROR %s.\n",$stmt->error);
+        return false;
+
+    }
 }
 
 ?>
